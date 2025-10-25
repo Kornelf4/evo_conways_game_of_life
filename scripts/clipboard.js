7cff082be1676865c684d2604f467629b0983548
kornelf <kornel.fab@gmail.com>
2025-10-25 22:38:24 +0200

    Added paste&copy rulesets

---
+++ scripts/clipboard.js
@@ -0,0 +1,22 @@
+document.getElementById("copyData").addEventListener("click", () => setClipboard(JSON.stringify(selectedRuleset)));
+document.getElementById("pasteData").addEventListener("click", async () => {
+    try {
+        let pastedData = JSON.parse(await navigator.clipboard.readText());
+        if(pastedData.checkedLocations === undefined || pastedData.conditionList === undefined) {
+            alert("Ruleset on clipboard is invalid.")
+        }
+        selectedRuleset.checkedLocations = pastedData.checkedLocations;
+        selectedRuleset.conditionList = pastedData.conditionList;
+        updateDisplay();
+    } catch(e) {
+        alert("Failed to paste from clipboard. (It might not work on itch.io)")
+    }
+});
+async function setClipboard(text) { //Copied from mbn docs
+  const type = "text/plain";
+  const clipboardItemData = {
+    [type]: text,
+  };
+  const clipboardItem = new ClipboardItem(clipboardItemData);
+  await navigator.clipboard.write([clipboardItem]);
+}
